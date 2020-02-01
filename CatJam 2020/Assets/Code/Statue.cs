84224b05dbbf23134e4cf13bc6002163dd37ead3
Chance <chance_lytle@yahoo.com>
2020-02-01 13:36:56 -0500

    base statue code

---
+++ CatJam 2020/Assets/Code/Statue.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Statue : MonoBehaviour
+{
+    public int health = 10;
+    public float intangibilityTime = 1f, flashTime = 0.05f;
+    private Coroutine intangible;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (intangible != null && collision.gameObject.GetComponent<CatMaster>()) {
+            health -= 1;
+            intangible = StartCoroutine(Intangibility());
+        }
+    }
+
+    IEnumerator Intangibility()
+    {
+        float timer = intangibilityTime;
+        while (timer > 0)
+        {
+            //ourSprite.enabled = false;
+            yield return new WaitForSeconds(flashTime);
+        }
+        //ourSprite.enabled = true
+        intangible = null;
+    }
+}
