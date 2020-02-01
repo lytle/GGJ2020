795ab61dc9ab8ebd7c8ae7b6ae835868abe8c3a8
Joey Yeo <yeojoey@gmail.com>
2020-02-01 16:34:02 -0500

    Fix generator script

--- CatJam 2020/Assets/Scripts/CatGenerator.cs
+++ CatJam 2020/Assets/Scripts/CatGenerator.cs
@@ -15,13 +15,13 @@ public class CatGenerator : MonoBehaviour
     public GameObject GenerateRandomCat()
     {
         GameObject newCat = Instantiate(CatPrefab);
-        newCat.GetComponent<Cat>().SetParts(GetRandomSprite(Bodies), GetRandomSprite(Faces), GetRandomSprite(Tails), Colors[Random.Range(0, Colors.Count - 1)]);
+        newCat.GetComponent<Cat>().SetParts(GetRandomSprite(Bodies), GetRandomSprite(Faces), GetRandomSprite(Tails), Colors[Random.Range(0, Colors.Count)]);
         return newCat;
     }
 
     Sprite GetRandomSprite(List<Sprite> sprites)
     {
-        return sprites[Random.Range(0, sprites.Count - 1)];
+        return sprites[Random.Range(0, sprites.Count)];
     }
 
 
