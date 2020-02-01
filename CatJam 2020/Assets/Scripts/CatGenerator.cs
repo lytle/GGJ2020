514c30fa6bbd5682be6c040d6f48465ce1b5375e
Joey Yeo <yeojoey@gmail.com>
2020-02-01 12:18:49 -0500

    Add cat and generator script

---
+++ CatJam 2020/Assets/Scripts/CatGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject CatPrefab;
+
+    [SerializeField]
+    List<Sprite> Bodies, Faces, Tails;
+
+    public GameObject GenerateRandomCat()
+    {
+        GameObject newCat = Instantiate(CatPrefab);
+        newCat.GetComponent<Cat>().SetParts(GetRandomSprite(Bodies), GetRandomSprite(Faces), GetRandomSprite(Tails));
+        return newCat;
+    }
+
+    Sprite GetRandomSprite(List<Sprite> sprites)
+    {
+        return sprites[Random.Range(0, sprites.Count - 1)];
+    }
+
+    private void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GenerateRandomCat();
+        }
+    }
+}
