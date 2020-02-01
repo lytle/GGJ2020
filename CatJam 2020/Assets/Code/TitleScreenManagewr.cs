9d1ff4c1d8fd6d9eae6bced912b63447f73d22bb
Chance <chance_lytle@yahoo.com>
2020-02-01 14:10:42 -0500

    ASDASD
    ASDASD

---
+++ CatJam 2020/Assets/Code/TitleScreenManagewr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManagewr : MonoBehaviour
+{
+    private Transform titleScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleScreen = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Start")) SceneManager.LoadScene("Chance Scene", LoadSceneMode.Additive);
+
+        titleScreen.transform.position = new Vector3(0f, 3f * Mathf.Sin(0.25f * Time.time), 1.0f);
+    }
+}
